018dedc36de55cc4a0392d24f229fc17f2811787
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-04-22 06:09:59 +0300

    fix comments, remove fallback for usize, bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-// Given a value "word", produces an integer in [0,p) without division.
+/// Given a value "word", produces an integer in [0,p) without division.
 #[inline(always)]
 pub fn fastrange_32(word: u32, p: u32) -> u32 {
     ((u64::from(word) * u64::from(p)) >> 32) as u32
 }
 
-// Given a value "word", produces an integer in [0,p) without division.
+/// Given a value "word", produces an integer in [0,p) without division.
 #[inline(always)]
 pub fn fastrange_64(word: u64, p: u64) -> u64 {
     ((word as u128 * p as u128) >> 64) as u64
 }
 
-// Given a value "word", produces an integer in [0,p) without division.
+/// Given a value "word", produces an integer in [0,p) without division.
 #[inline(always)]
 pub fn fastrange_usize(word: usize, p: usize) -> usize {
     #[cfg(target_pointer_width = "32")]
@@ -23,9 +23,13 @@ pub fn fastrange_usize(word: usize, p: usize) -> usize {
         fastrange_64(word as u64, p as u64) as usize
     }
 
-    #[cfg(all(not(target_pointer_width = "64"), not(target_pointer_width = "32")))]
+    #[cfg(all(
+        not(target_pointer_width = "64"),
+        not(target_pointer_width = "32"),
+        not(target_pointer_width = "16")
+    ))]
     {
-        word % p
+        compile_error!("target not supported.");
     }
 }
 
