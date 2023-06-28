290910b42cb14f355fc242b56c10f9747be6eeca
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-06-28 17:30:27 +0600

    remove usize version, bump to 0.1

--- src/lib.rs
+++ src/lib.rs
@@ -12,25 +12,6 @@ pub fn fastrange_64(word: u64, p: u64) -> u64 {
     ((word as u128 * p as u128) >> 64) as u64
 }
 
-/// Given a value "word", produces an integer in [0,p) without division.
-#[inline(always)]
-pub fn fastrange_usize(word: usize, p: usize) -> usize {
-    #[cfg(target_pointer_width = "32")]
-    {
-        fastrange_32(word as u32, p as u32) as usize
-    }
-
-    #[cfg(target_pointer_width = "64")]
-    {
-        fastrange_64(word as u64, p as u64) as usize
-    }
-
-    #[cfg(all(not(target_pointer_width = "64"), not(target_pointer_width = "32")))]
-    {
-        compile_error!("target not supported.");
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
